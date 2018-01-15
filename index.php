009b5ea3bd8cabd54ccc5cd8f1b4421570ddff77
Avik Nigam <business.aviknigam@gmail.com>
2018-01-16 05:19:56 +1100

    Initial Commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+require __DIR__ . '/core/init.php';
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<img src="https://images.unsplash.com/photo-1476514525535-07fb3b4ae5f1" alt="">
+    
+</body>
+</html>
